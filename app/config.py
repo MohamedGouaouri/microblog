a2523b35fcc875fb6ee8b0a9578af2204e347d2c
MohamedGouaouri <im_gouaouri@esi.dz>
2020-07-30 13:55:10 +0100

    finish email support

--- app/config.py
+++ app/config.py
@@ -13,6 +13,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['mohamedgouari2000@gmail.com']
+    ADMINS = ['admin@localhost.com']
 
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 10
